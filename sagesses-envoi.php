16b576f0b8c81a1944cb1e1188c1a24e5c8bf0b1
skotperez <voraginebramante@gmail.com>
2017-10-31 07:04:54 +0100

    Fixes error while saving options in form of dashboard setting page

--- sagesses-envoi.php
+++ sagesses-envoi.php
@@ -11,7 +11,6 @@ License: GPLv3
 // VARIABLES
 $subjects_count = 10; // Maximum number of subjects for the emails to choose from
 $addresses_count = 20; // Maximum number of email addresses to send emails to
-$content_cpt = 'email';
 
 // ADD IMAGE SIZE FOR EMAIL CONTENT
 add_action( 'init', 'sgs_emails_image_size' );
@@ -40,33 +39,56 @@ function sgs_emails_dashboard_page() {
 add_action( 'admin_init', 'sgs_emails_register_settings' );
 function sgs_emails_register_settings() {
 
-	// subjects list setting
-	register_setting( 'sgs_emails_settings_subjects_group', 'sgs_emails_settings_subjects' );
-	add_settings_section( 'sgs_emails_settings_subjects_section', __('Subjects','sgs_emails'), 'sgs_emails_settings_subjects_section_callback', 'sagesses_emails' );
+	register_setting( 'sgs_emails_settings_group', 'sgs_emails_settings' );
 
-	add_settings_field( 'sgs_emails_settings_subjects_field', __('List of subjects','sgs_emails'), 'sgs_emails_settings_subjects_callback', 'sagesses_emails', 'sgs_emails_settings_subjects_section' );
+	// post type settings
+	add_settings_section( 'sgs_emails_settings_ptype_section', __('Post type','sgs_emails'), 'sgs_emails_settings_ptype_section_callback', 'sagesses_emails' );
+	add_settings_field( 'sgs_emails_settings_ptype', __('Post type for email content','sgs_emails'), 'sgs_emails_settings_ptype_callback', 'sagesses_emails', 'sgs_emails_settings_ptype_section' );
+
+	// subjects list setting
+	add_settings_section( 'sgs_emails_settings_subjects_section', __('Subjects','sgs_emails'), 'sgs_emails_settings_subjects_section_callback', 'sagesses_emails' );
+	add_settings_field( 'sgs_emails_settings_subjects', __('List of subjects','sgs_emails'), 'sgs_emails_settings_subjects_callback', 'sagesses_emails', 'sgs_emails_settings_subjects_section' );
 
 	// email addresses list setting
-	register_setting( 'sgs_emails_settings_addresses_group', 'sgs_emails_settings_addresses' );
 	add_settings_section( 'sgs_emails_settings_addresses_section', __('Addresses','sgs_emails'), 'sgs_emails_settings_addresses_section_callback', 'sagesses_emails' );
-
-	add_settings_field( 'sgs_emails_settings_addresses_field', __('List of addresses','sgs_emails'), 'sgs_emails_settings_addresses_callback', 'sagesses_emails', 'sgs_emails_settings_addresses_section' );
+	add_settings_field( 'sgs_emails_settings_addresses', __('List of addresses','sgs_emails'), 'sgs_emails_settings_addresses_callback', 'sagesses_emails', 'sgs_emails_settings_addresses_section' );
 
 }
 
 // CALLBACK FUNCTIONS
 
+// post type
+function sgs_emails_settings_ptype_section_callback() {
+	echo __('Choose a post type to feed the emails with content.','sgs_emails');
+}
+
+function sgs_emails_settings_ptype_callback() {
+	global $wp_post_types;
+	$args = array(
+		'public' => true
+	);
+	$ptypes = get_post_types($args);
+	$settings = (array) get_option( 'sgs_emails_settings' );
+	$ptype = esc_attr( $settings['sgs_emails_settings_ptype'] );
+	$options = '<option value=""></option>';
+	foreach ( $ptypes as $pt ) {
+		$options .= ( $pt == $ptype ) ? '<option value="'.$pt.'" selected>'.$pt.'</option>' : '<option value="'.$pt.'">'.$pt.'</option>';
+	}
+	echo "<select name='sgs_emails_settings[sgs_emails_settings_ptype]'>".$options."</select>";
+}
+
 // subjects list
 function sgs_emails_settings_subjects_section_callback() {
 	echo __('List of subjects for emails. When an email is sent, its subject will be chosen randomly from this list.','sgs_emails');
 }
+
 function sgs_emails_settings_subjects_callback() {
-	$settings = (array) get_option( 'sgs_emails_settings_subjects' );
+	$settings = (array) get_option( 'sgs_emails_settings' );
 	global $subjects_count;
 	$count = 0;
 	while ( $count < $subjects_count ) {
-		$subject = esc_attr( $settings['sgs_emails_settings_subjects_field'][$count] );
-		echo "<input type='text' name='sgs_emails_settings_subjects[sgs_emails_settings_subjects_field][".$count."]' value='$subject' />";
+		$subject = esc_attr( $settings['sgs_emails_settings_subjects'][$count] );
+		echo "<input type='text' name='sgs_emails_settings[sgs_emails_settings_subjects][".$count."]' value='$subject' />";
 		$count++;
 	}
 }
@@ -75,13 +97,14 @@ function sgs_emails_settings_subjects_callback() {
 function sgs_emails_settings_addresses_section_callback() {
 	echo __('List of addresses to send emails to.','sgs_emails');
 }
+
 function sgs_emails_settings_addresses_callback() {
-	$settings = (array) get_option( 'sgs_emails_settings_addresses' );
+	$settings = (array) get_option( 'sgs_emails_settings' );
 	global $addresses_count;
 	$count = 0;
 	while ( $count < $addresses_count ) {
-		$address = esc_attr( $settings['sgs_emails_settings_addresses_field'][$count] );
-		echo "<input type='text' name='sgs_emails_settings_addresses[sgs_emails_settings_addresses_field][".$count."]' value='$address' />";
+		$address = esc_attr( $settings['sgs_emails_settings_addresses'][$count] );
+		echo "<input type='text' name='sgs_emails_settings[sgs_emails_settings_addresses][".$count."]' value='$address' />";
 		$count++;
 	}
 
@@ -92,8 +115,7 @@ function sgs_emails_dashboard_page_output() { ?>
 	<div class="wrap">
 		<h2><?php _e('Sagesses send emails tool','sgs'); ?></h2>
 		<form method="post" action="options.php">
-			<?php settings_fields( 'sgs_emails_settings_subjects_group' ); ?>
-			<?php settings_fields( 'sgs_emails_settings_addresses_group' ); ?>
+			<?php settings_fields( 'sgs_emails_settings_group' ); ?>
 			<?php do_settings_sections( 'sagesses_emails' ); ?>
 			<?php submit_button(); ?>
 		</form>
